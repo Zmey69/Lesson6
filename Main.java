894a4e9d4826260d9006e4421be547a3f5b8d55e
Zmey69 <125044870+Zmey69@users.noreply.github.com>
2023-04-21 00:06:26 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\t\t\t***Программа определения весов товаров***\n\n\n");
-
-        System.out.println("Введите первый вес: ");
-        double first = new Scanner(System.in).nextDouble();
-
-        System.out.println("Введите второй вес: ");
-        double second = new Scanner(System.in).nextDouble();
-
-        System.out.println("Введите третий вес: ");
-        double third = new Scanner(System.in).nextDouble();
-
-        double greatest = 0;
-        double average = 0;
-        double smallest = 0;
-
-        //todo дописать логику программы ниже.
-
-        greatest = first > second && first > third ? first : second > third ? second : third;
-
-        smallest = first < second && first < third ? first : second < third ? second : third;
-
-        average = (first > second && first < third) || (first < second && first > third) ? first
-                : (second > first && second < third) || (second < first && second > third) ? second
-                : third;
-
-
-        //todo
-
-        System.out.println("Наибольший вес: " + greatest);
-        System.out.println("Средний вес: " + average);
-        System.out.println("Наименьший вес: " + smallest);
+        //1. Сделать так, чтобы нижний sout выводил true. Для этого используйте логические операторы вместо ...
+        System.out.println(5 <= 10);
+        //2. Сделать так, чтобы нижний sout выводил false. Для этого используйте логические операторы вместо ...
+        System.out.println(5 >= 10);
+        //3. Сделать так, чтобы нижний sout выводил true. Для этого используйте логические операторы вместо ...
+        System.out.println(10 >= 10);
+        //4. Сделать так, чтобы нижний sout выводил false. Для этого используйте логические операторы вместо ...
+        System.out.println(5 != 5);
+        //5. Сделать так, чтобы нижний sout выводил true. Для этого используйте логические операторы вместо ...
+        System.out.println(true || false);
+        //6. Сделать так, чтобы нижний sout выводил false. Для этого используйте логические операторы вместо ...
+        System.out.println(true && false);
     }
 }
